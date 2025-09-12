e59ce1c8ceb208a2b7ad6933f96b31d90402bb96
Prateek Kumar Srivastav <pankaj111sri@gmail.com>
2025-09-13 00:31:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
 import Soil from './pages/Soil'
 import Home from './pages/Home'
